a7553010ffeab6a99786ebcf79c7605c1b51c672
Nestor Compaore <devfull100@gmail.com>
2023-07-05 13:59:42 +0000

    3td commit

--- screens/Item.jsx
+++ screens/Item.jsx
@@ -1,22 +1,24 @@
 import React, { useState, useEffect } from 'react'
 import {
   View,
+  Share,
   Text,
   ActivityIndicator,
   Image,
   TouchableOpacity
-} from 'react-native'
+} from 'react-native';
 import { Ionicons } from '@expo/vector-icons'
 import { AntDesign } from '@expo/vector-icons'
-import { BlurView } from 'expo-blur';
-import * as Location from 'expo-location'
+import { BlurView } from 'expo-blur'
+import { Entypo } from '@expo/vector-icons'
+import { Feather } from '@expo/vector-icons'
+import * as Location from 'expo-location';
 import * as Expo from 'expo'
 import * as FileSystem from 'expo-file-system'
 import * as Permissions from 'expo-permissions'
 import moment from 'moment'
 import * as MediaLibrary from 'expo-media-library'
 import { getItemById } from '../sanity'
-import ManageWallpaper, { TYPE } from 'react-native-manage-wallpaper'
 import { urlFor } from '../sanity'
 import { useNavigation } from '@react-navigation/native'
 import { SafeAreaView } from 'react-native-safe-area-context'
@@ -38,43 +40,56 @@ export default function Item ({ route }) {
     }, 2000)
   }, [])
 
-
-
   const handleDownload = async () => {
-    let date = moment().format('YYYYMMDDhhmmss');
-    let fileUri = FileSystem.documentDirectory + `${date}.jpg`;
+    let date = moment().format('YYYYMMDDhhmmss')
+    let fileUri = FileSystem.documentDirectory + `${date}.jpg`
     try {
-      const { status } = await Permissions.askAsync(Permissions.MEDIA_LIBRARY);
-  
+      const { status } = await Permissions.askAsync(Permissions.MEDIA_LIBRARY)
+
       if (status === 'granted') {
         const res = await FileSystem.downloadAsync(
           urlFor(item?.image).url(),
           fileUri
-        );
-        saveFile(res.uri);
+        )
+        saveFile(res.uri)
       } else if (status === 'denied') {
         const { status: newStatus } = await Permissions.askAsync(
           Permissions.MEDIA_LIBRARY
-        );
-  
+        )
+
         if (newStatus === 'granted') {
           const res = await FileSystem.downloadAsync(
             urlFor(item?.image).url(),
             fileUri
-          );
-          saveFile(res.uri);
+          )
+          saveFile(res.uri)
         } else {
-          alert('Please allow permissions to download');
+          alert('Please allow permissions to download')
         }
       }
     } catch (err) {
-      console.log('FS Err: ', err);
+      console.log('FS Err: ', err)
+    }
+  }
+
+  const shareImage = async () => {
+    if (item && item.image) {
+      const imageUrl = urlFor(item.image).url();
+      try {
+        await Share.share({
+          message: 'Share image',
+          url: imageUrl
+        });
+      } catch (error) {
+        console.log(error);
+      }
     }
   };
   
+  const setWallpaper = async () => {}
 
   const saveFile = async fileUri => {
-    const { status } = await MediaLibrary.requestPermissionsAsync();
+    const { status } = await MediaLibrary.requestPermissionsAsync()
     if (status === 'granted') {
       try {
         const asset = await MediaLibrary.createAssetAsync(fileUri)
@@ -93,18 +108,6 @@ export default function Item ({ route }) {
       alert('please allow permissions to download')
     }
   }
-
-  // const handleSetAsWallpaper = async () => {
-  //   try {
-  //     await ManageWallpaper.setWallpaper(
-  //       { uri: urlFor(item?.image).url() },
-  //       TYPE.BOTH
-  //     )
-  //     alert('Image set as wallpaper')
-  //   } catch (err) {
-  //     console.log('Set wallpaper err: ', err)
-  //   }
-  // }
 
   return (
     <View className='flex-1 items-center justify-center bg-[#04020d] relative'>
@@ -136,31 +139,15 @@ export default function Item ({ route }) {
                   intensity={60}
                   tint='dark'
                 >
-                  <View className='flex items-start justify-between gap-3'>
-                    <Text className='text-3xl text-white font-bold'>
-                      {item?.title}
-                    </Text>
-                    <Text className='text-white font-bold'>
-                      {item?.description}
-                    </Text>
-                  </View>
-                  <View className='flex-row justify-between items-center'>
-                    <TouchableOpacity onPress={handleDownload}>
-                      <AntDesign 
-                      name='save' 
-                      size={24} 
-                      color='white' 
-                      />
-                    </TouchableOpacity>
-                    {/* <TouchableOpacity onPress={handleSetAsWallpaper}>
-                      <Ionicons
-                        name='image'
-                        size={24}
-                        color='white'
-                        style={{ marginLeft: 16 }}
-                      />
-                    </TouchableOpacity> */}
-                  </View>
+                  <TouchableOpacity onPress={shareImage}>
+                    <Entypo name='share' size={24} color='white' />
+                  </TouchableOpacity>
+                  <TouchableOpacity onPress={setWallpaper}>
+                    <Feather name='image' size={24} color='white' />
+                  </TouchableOpacity>
+                  <TouchableOpacity onPress={handleDownload}>
+                    <AntDesign name='save' size={24} color='white' />
+                  </TouchableOpacity>
                 </BlurView>
               </View>
             </View>
